8d8d966b5719c368ff333972cf43fe803364fc2b
Skyler Giannetti <sjg5901@psu.edu>
2023-07-09 00:29:02 +0000

    Bank Account Project complete

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        this.balance += amount;
     }
 
     /**
@@ -32,7 +32,7 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        this.balance = (this.balance - amount >= 0) ? (this.balance - amount) : this.balance;
     }
 
     /**
@@ -40,6 +40,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return this.balance;
     }
 }
